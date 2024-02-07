05592f3198f0c083193246ccd21d892abe0f6eb2
ehtjsdlfqhd <ehtjsdlfqhd@naver.com>
2024-02-07 19:52:49 +0900

    Fix: OneToMany 코드 작성, ToString 무한 루프 방지 코드 작성

--- src/main/java/com/sh/app/theater/entity/Theater.java
+++ src/main/java/com/sh/app/theater/entity/Theater.java
@@ -1,10 +1,12 @@
 package com.sh.app.theater.entity;
 
+import com.sh.app.cinema.entity.Cinema;
+import com.sh.app.schedule.entity.Schedule;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "theater")
@@ -12,17 +14,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = "schedules")
 public class Theater {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "cinema_id")
-//    private Cinema cinema;
-    @Column(nullable = false)
-    private Long cinemaId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "cinema_id")
+    private Cinema cinema;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
     private int seat;
+
+    @OneToMany(mappedBy = "theater", fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<Schedule> schedules = new ArrayList<>();
 }
