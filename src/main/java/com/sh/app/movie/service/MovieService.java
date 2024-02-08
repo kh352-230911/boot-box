fa7797dd60a46ec8b0efab12bb48f4ee7d00553f
ryujeongho <ryujh0326@gmail.com>
2024-02-08 18:32:52 +0900

    Feat: movieList.do 추가

---
+++ src/main/java/com/sh/app/movie/service/MovieService.java
@@ -0,0 +1,10 @@
+package com.sh.app.movie.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MovieService {
+}
