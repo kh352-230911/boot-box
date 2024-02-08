e7a5a20ef3a002a839a1f02d452c873ece30e65e
dlachdla <chyolim4692@naver.com>
2024-02-08 09:24:25 +0900

    Feat: movie test작성

--- src/main/java/com/sh/app/movie/entity/Movie.java
+++ src/main/java/com/sh/app/movie/entity/Movie.java
@@ -1,10 +1,17 @@
 package com.sh.app.movie.entity;
 
+import com.sh.app.genre.entity.Genre;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "movie")
@@ -15,6 +22,7 @@ import lombok.NoArgsConstructor;
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "movie_id")
     private Long id;
     @Column(nullable = false)
     private String title;
@@ -30,4 +38,16 @@ public class Movie {
     private String actor;
     private String summary;
     private double advanceReservation;
+
+   @ManyToMany
+   @JoinTable(
+           name = "movie_genre",
+           joinColumns = @JoinColumn(name = "movie_id"),
+           inverseJoinColumns = @JoinColumn(name = "genre_id"))
+   @Builder.Default
+   private Set<Genre> genres = new LinkedHashSet<>();
+
+    public void addMovieGenre(Genre genre) {
+        this.genres.add(genre);
+    }
 }
--- src/main/java/com/sh/app/movie/repository/MovieRepository.java
+++ src/main/java/com/sh/app/movie/repository/MovieRepository.java
@@ -2,7 +2,27 @@ package com.sh.app.movie.repository;
 
 import com.sh.app.movie.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+@Query(value = """
+    select 
+        *
+    from(select 
+            *
+        from 
+            movie
+        order by 
+            advance_reservation desc)
+    where 
+        rownum between 1 and 5
+    """, nativeQuery = true)
+    List<Movie> findFirst5ByOrderByAdvanceReservation();
 
+    @Query("select m, g from Movie m join fetch m.genres g where g.genreList = :genreList")
+    List<Movie> findByGenreList(String genreList);
 }
