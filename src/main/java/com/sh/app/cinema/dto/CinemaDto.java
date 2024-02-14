13355c07ebd9a895643aa0abfda632ce334ad69d
jindamgom <update_set@naver.com>
2024-02-14 19:39:16 +0900

    feat : 0214 cinemaDto (건드리지도 않았는데 자꾸 커밋에 뜸)

--- src/main/java/com/sh/app/cinema/dto/CinemaDto.java
+++ src/main/java/com/sh/app/cinema/dto/CinemaDto.java
@@ -1,17 +1,25 @@
 package com.sh.app.cinema.dto;
 
 import com.sh.app.location.entity.Location;
-
+import com.sh.app.theater.entity.Theater;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
-public class CinemaDto {
+public class CinemaDTO {
     private Long id;
     private String region_cinema;
     private int theater_number;
     private String address;
-    private int location_lo;
-    private int location_la;
+    private int location_lo; // 지도 경도
+    private int location_la; // 지도 위도
     private String phone;
-    private String location_name; // Location#location_name
+
+    private Location location;
+
+    private List<Theater> theaters = new ArrayList<>();
 }
