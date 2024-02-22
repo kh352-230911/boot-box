93526755eba71a263107a585a879b2ae0168c25f
ehtjsdlfqhd <ehtjsdlfqhd@naver.com>
2024-02-22 19:09:53 +0900

    Feat: 삭제요청 비동기처리 js 추가

--- src/main/resources/static/js/admin/adminTheaters.js
+++ src/main/resources/static/js/admin/adminTheaters.js
@@ -10,3 +10,32 @@ $(".back").click(function (){
    $(".createModal").css("display", "none");
    $(".deleteModal").css("display", "none");
 });
+
+$("#insert").click(function () {
+    alert("상영관 등록이 완료되었습니다.");
+});
+
+
+$("#deleteSearch").click(function () {
+    const deleteId = $("#deleteId").val();
+    const deleteName = $("#deleteName").val();
+    const confirmText = confirm("정말 삭제하시겠습니까?");
+    if (confirmText) {
+        $.ajax({
+            url:"deleteTheater",
+            method:"DELETE",
+            data:{
+                deleteId, deleteName
+            },
+            success(){
+                alert("삭제가 완료되었습니다.");
+            },
+            error() {
+                alert("삭제에 실패했습니다.");
+            }
+        });
+    }
+    else {
+        alert("삭제를 취소했습니다.")
+    }
+});
