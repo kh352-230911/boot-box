971314638ca63d89754af6b51b506b6882571e1c
dlachdla <chyolim4692@naver.com>
2024-02-23 20:50:17 +0900

    Fix: CinemaDto 수정

--- src/main/java/com/sh/app/cinema/dto/CinemaDto.java
+++ src/main/java/com/sh/app/cinema/dto/CinemaDto.java
@@ -1,27 +1,25 @@
 package com.sh.app.cinema.dto;
 
 import com.sh.app.location.entity.Location;
-import com.sh.app.memberLikeCinema.entity.MemberLikeCinema;
 import com.sh.app.theater.entity.Theater;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class CinemaDto {
+public class CinemaDTO {
     private Long id;
     private String region_cinema;
     private int theater_number;
     private String address;
-    private Double location_lo; // 지도 경도
-    private Double location_la; // 지도 위도
+    private int location_lo; // 지도 경도
+    private int location_la; // 지도 위도
     private String phone;
 
     private Location location;
-    private String location_name;
 
     private List<Theater> theaters = new ArrayList<>();
-
-    private List<MemberLikeCinema> memberLikeCinemas = new ArrayList<>();
 }
