20845d3238c845baa9b30993d812012fdcdce9da
dlachdla <chyolim4692@naver.com>
2024-03-30 19:21:12 +0900

    Fix : CustomSuccessHandler 로그인 redirectUrl버그 수정

--- src/main/java/com/sh/app/auth/handler/CustomSuccessHandler.java
+++ src/main/java/com/sh/app/auth/handler/CustomSuccessHandler.java
@@ -3,7 +3,6 @@ package com.sh.app.auth.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -18,39 +17,43 @@ import java.io.IOException;
 @Slf4j
 public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 
-
     private RequestCache requestCache = new HttpSessionRequestCache();
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException {
 
-        //로그인 후 처리 작업
-        //1.알림테이블에서 미확인 알림을 조회
-        //2. ..
+        // 로그인 후 처리 작업
+        // 1.알림테이블에서 미확인 알림을 조회
+        // 2. ..
 
-        /**
-         * 기본 redirect 페이지 지정
-         */
-        String targetUrl = "/";
+        String targetUrl = determineTargetUrl(request);
 
-        /**
-         * 인증전 접근시도한 페이지가 session에 SPRING_SECURITY_SAVED_REQUEST 속성명으로 저장되어 있다.
-         */
-        SavedRequest savedRequest = (SavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
-        if(savedRequest != null) {
-            System.out.println("=====================이전페이지가 저장되어있어요!=====================");
-            targetUrl = savedRequest.getRedirectUrl(); //로그인 한 후 해당 페이지로 접근
-        }
-        else {
-            System.out.println("=====================이전페이지가 저장되어있지 않아요!=====================");
-        }
         log.debug("targetUrl = {}", targetUrl);
 
         redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 
+    protected String determineTargetUrl(HttpServletRequest request) {
+        SavedRequest savedRequest = requestCache.getRequest(request, null);
 
+        if (savedRequest != null) {
+            String redirectUrl = savedRequest.getRedirectUrl();
+            System.out.println("=====================이전페이지가 저장되어있어요!=====================");
+            // 로그인 페이지 또는 로그인 페이지로 리다이렉트하는 URL인지 확인
+            if (!isLoginUrl(redirectUrl)) {
+                return redirectUrl;
+            }
+        } else {
+            System.out.println("=====================이전페이지가 저장되어있지 않아요!=====================");
+        }
+
+        // 기본 페이지로 리다이렉션
+        return "/";
+    }
+
+    protected boolean isLoginUrl(String url) {
+        return url.contains("/login");
+    }
 }
