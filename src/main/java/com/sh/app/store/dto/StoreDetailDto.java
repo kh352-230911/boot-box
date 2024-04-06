aba1f4afa7e95bc96b11f705dbc02d41e100b518
ryujeongho <ryujh0326@gmail.com>
2024-04-06 21:34:55 +0900

    Feat: 스토어 추가

---
+++ src/main/java/com/sh/app/store/dto/StoreDetailDto.java
@@ -0,0 +1,28 @@
+package com.sh.app.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreDetailDto {
+    private Long id;
+
+    private String type;
+
+    private String imageUrl;
+
+    private String name;
+
+    private Long price;
+
+    private String description;
+
+    private LocalDate expirationPeriod;
+}
