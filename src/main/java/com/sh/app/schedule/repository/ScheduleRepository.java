c545cc136f6789d3db40a9c9ef1775325fb71bba
jindamgom <update_set@naver.com>
2024-06-10 13:58:29 +0900

    fix : server utc 시간대 이슈 - currentTimeStamp에서 zone으로 수정

--- src/main/java/com/sh/app/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/sh/app/schedule/repository/ScheduleRepository.java
@@ -8,13 +8,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    LocalDateTime localDateTime = LocalDateTime.now();
+    ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of("Asia/Seoul"));
+    Timestamp timestamp = Timestamp.valueOf(zonedDateTime.toLocalDateTime());
 
     @Query("from Schedule s join fetch s.theater t where t.id = :theaterId")
     List<Schedule> findByTheaterId(Long theaterId);
@@ -53,9 +61,11 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
         JOIN schedule s ON s.theater_id = t.id
         JOIN movie_list ml ON ml.cinema_id = c.id
         JOIN movie m ON m.id = ml.movie_id AND m.id = s.movie_id
-        WHERE (c.id = :id AND s.sch_date = :schDate AND s.approve='Y') AND s.time > CURRENT_TIMESTAMP ORDER BY t.id,s.time""", nativeQuery = true)
+        WHERE (c.id = :id AND s.sch_date = :schDate AND s.approve='Y') AND s.time > :localDateTime
+        ORDER BY t.id,s.time""", nativeQuery = true)
     List<IScheduleInfoDto> findScheduleDetailsByDateAndCinemaId(@Param("id") Long id,
-                                                                @Param("schDate") LocalDate schDate);
+                                                                @Param("schDate") LocalDate schDate,
+                                                                @Param("localDateTime")LocalDateTime localDateTime);
 
     //  WHERE (c.id = :id AND s.sch_date = :schDate) AND s.time > CURRENT_TIMESTAMP """, nativeQuery = true)
 
@@ -82,8 +92,9 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
         JOIN schedule s ON s.theater_id = t.id
         JOIN movie_list ml ON ml.cinema_id = c.id
         JOIN movie m ON m.id = ml.movie_id AND m.id = s.movie_id
-        WHERE (m.id= :movieId AND c.id = :cinemaId AND s.sch_date = :schDate AND s.approve='Y') AND s.time > CURRENT_TIMESTAMP ORDER BY t.id,s.time""", nativeQuery = true)
-    List<IScheduleInfoDto> findScheduleDetailsByDateAndCinemaId_2(Long movieId, Long cinemaId, LocalDate schDate);
+        WHERE (m.id= :movieId AND c.id = :cinemaId AND s.sch_date = :schDate AND s.approve='Y') AND s.time > :localDateTime 
+        ORDER BY t.id,s.time""", nativeQuery = true)
+    List<IScheduleInfoDto> findScheduleDetailsByDateAndCinemaId_2(Long movieId, Long cinemaId, LocalDate schDate,LocalDateTime localDateTime);
 
     Long countByMovieId(Long movieId);
 
@@ -92,8 +103,8 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
 
     //해당 영화, 지점으로 현재기준으로 미래인 상영스케쥴 조회(단, 승인된 스케쥴이여야 한다)
-    @Query("SELECT s FROM Schedule s JOIN s.theater t WHERE t.cinema.id = :cinemaId AND s.movie.id = :movieId AND s.approve='Y' AND s.time > CURRENT_TIMESTAMP ")
-    List<Schedule> searchMovieSchedule(Long cinemaId, Long movieId);
+    @Query("SELECT s FROM Schedule s JOIN s.theater t WHERE t.cinema.id = :cinemaId AND s.movie.id = :movieId AND s.approve='Y' AND s.time > :localDateTime ")
+    List<Schedule> searchMovieSchedule(Long cinemaId, Long movieId,LocalDateTime localDateTime);
 
 
     //0428 극장에서 상영일정 클릭해서 바로 예매페이지 넘어가는 경우! 상영일정id 하나만으로 조회 시도
